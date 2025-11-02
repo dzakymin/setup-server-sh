55a678a1907b6233202a8aa4597adc43483a0c0b
jakimin <jakimin@git.com>
2025-11-02 23:56:34 +0700

    slighly changes

--- setup-serv.sh
+++ setup-serv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Provisioning script for kubernetes...."
+echo "========Provisioning script for kubernetes============="
 
 if docker --version &> /dev/null; then
 	echo "docker sudah terinstall"
 else
-	echo "memulai installasi docker..."
+	echo "[+]memulai installasi docker..."
 	# Add Docker's official GPG key:
 	 apt-get update
 	 apt-get install ca-certificates curl -y
@@ -24,40 +24,39 @@ else
 	 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 	 systemctl enable docker
 	 systemctl start docker
-	echo "Berhasil menginstall docker...."
+	echo "[+]Berhasil menginstall docker...."
 fi
 
 
 
-echo "installing kubectl"
+echo "=======installing kubectl========="
 if kubectl --version &> /dev/null; then
-	echo "Kubectl sudah terinstall, siap digunakan"
+	echo "[+]Kubectl sudah terinstall, siap digunakan"
 else
-	echo "memulai installasi kubectl...[stage 1]"
+	echo "[+]memulai installasi kubectl...[stage 1]"
 	sudo apt-get update
 	# apt-transport-https may be a dummy package; if so, you can skip that package
 	sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 
-	echo "memulai installasi kubectl...[stage 2]"
+	echo "[+]memulai installasi kubectl...[stage 2]"
 	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 	sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
 
-	echo "memulai installasi kubectl...[stage 3]"
+	echo "[+]memulai installasi kubectl...[stage 3]"
 	# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
 	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 	chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctl
 
-	echo "memulai installasi kubectl...[stage 4]"
+	echo "[+]memulai installasi kubectl...[stage 4]"
 	apt-get update
 	apt-get install -y kubectl
 fi
 
 
 
-echo "k3s installation...."
+echo "========k3s installation========"
 read -p "role node di kubernetes (cp | cp-sec | wk) : " role
-cpnode1=082121990261
-wk_ip=081312124020
+#cpnode1=082121990261
 k3stoken="a3ViZXJuYXRlc0BzdmlwYnNlcnYK"
 
 if systemctl is-active --quiet k3s.service; then
@@ -67,17 +66,19 @@ else
 	 	export K3S_TOKEN=$k3stoken
 		curl -sfL https://get.k3s.io | sh -s - server --disable traefik --disable servicelb --disable local.storage --docker --cluster-init
 	elif [ "$role" = "cp-sec" ]; then
+		read -p "Masukan ip server control plane utama : " cpnode1
 		export K3S_TOKEN=$k3stoken
 		curl -sfL https://get.k3s.io | sh -s - server --server https://$cpnode1:6443 --disable traefik --disable servicelb --disable local.storage --docker
 	elif [ "$role" = "wk" ]; then
+		read -p "Masukan ip server control plane utama : " cpnode1
 		export K3S_TOKEN=$k3stoken
-		curl -sfL https://get.k3s.io | sh -s - agent --docker --server https://$wk_ip:6443
+		curl -sfL https://get.k3s.io | sh -s - agent --docker --server https://$cpnode1:6443
 	else
 		echo "error occured"
 	fi
 fi
-systemctl restart k3s
 
+systemctl restart k3s
 echo "export config"
 read -p "masukan home directory biasa anda : " dirhome
 if [ ! -d "$dirhome/.kube" ]; then
@@ -98,4 +99,17 @@ if ! kubectl get node &> /dev/null; then
 	echo "terdapat error pada k3s anda"
 
 fi
-echo "Provisioning end..."
+
+echo "=====helm installation====="
+if kubectl get node &> /dev/null | grep -q control-plane; then
+  	read -p "masukan"
+	apt-get install curl gpg apt-transport-https --yes
+	curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor |  tee /usr/share/keyrings/helm.gpg > /dev/null
+	echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" |  tee /etc/apt/sources.list.d/helm-stable-debian.list
+	apt-get update
+	apt-get install helm
+else
+	echo "your helm already installed"
+fi
+
+echo "Provisioning end....."
