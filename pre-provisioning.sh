991fd684409bd5c50904c74e7d18f37da0663ba6
jakimin <jakimin@git.com>
2025-11-03 07:44:03 +0700

    sudah kelar

--- pre-provisioning.sh
+++ pre-provisioning.sh
@@ -2,18 +2,28 @@
 
 set -e
 
-echo "[+] installing open-iscsi...."
+echo "[+]installing jq lib..."
+sudo apt install jq -y
+
+echo "[+]Installing open-iscsi...."
 sudo apt install open-iscsi -y
 
-echo "[+]enabling iscsi_tcp for kernel longhorn purposes...."
+echo "[+]Enabling iscsi_tcp for kernel longhorn purposes...."
 sudo modprobe iscsi_tcp
 
-echo "[+]enabling iscsi service...."
+echo "[+]Enabling iscsi service...."
 sudo systemctl enable iscsi.service
 sudo systemctl start iscsi.service
 
-echo "[+]installing nfs-common...."
+echo "[+]Installing nfs-common...."
 sudo apt install nfs-common -y
 
+echo "[+]Installing cryptsetup...."
+sudo apt install cryptsetup -y
 
+echo "[+]Disabling multipath sockerd"
+if sudo systemctl stop multipath multipathd.service; then
+	echo "Your system doesn't have multipath, skipp"
+else
+	echo "shutdown your multipath"
 
