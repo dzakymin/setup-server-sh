af92c8cc525a61c00bbff233023259522cee4fbf
jakimin <jakimin@git.com>
2025-11-03 07:29:03 +0700

    sudah kelar

--- pre-provisioning.sh
+++ pre-provisioning.sh
@@ -3,23 +3,17 @@
 set -e
 
 echo "[+] installing open-iscsi...."
-apt install open-iscsi -y
+sudo apt install open-iscsi -y
 
 echo "[+]enabling iscsi_tcp for kernel longhorn purposes...."
-modprobe iscsi_tcp
+sudo modprobe iscsi_tcp
 
 echo "[+]enabling iscsi service...."
-systemctl enable iscsi.service
-systemctl start iscsi.service
+sudo systemctl enable iscsi.service
+sudo systemctl start iscsi.service
 
 echo "[+]installing nfs-common...."
-apt install nfs-common -y
+sudo apt install nfs-common -y
 
-
-if ! curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.7.1/scripts/environment_check.sh | sudo bash &> /dev/null; then
-	echo "[*]Ada package yang kurang unutk melakukan provissioning"
-else
-	echo "[+]Pre-provisioning berhasil"
-fi
 
 
